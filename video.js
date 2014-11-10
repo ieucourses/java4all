9d45278161c8dcab17cb5ce8d6f2a246b0c5dd61
ieucourses <ieucourses@gmail.com>
2014-11-10 12:46:27 +0200

    video.js

--- video.js
+++ video.js
@@ -1,25 +1,22 @@
-jQuery(document).ready(function() {
-
-	$(".video").click(function() {
-		$.fancybox({
-			'padding'		: 0,
-			'autoScale'		: false,
-			'transitionIn'	: 'none',
-			'transitionOut'	: 'none',
-			'title'			: this.title,
-			'width'			: 640,
-			'height'		: 385,
-			'href'			: this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
-			'type'			: 'swf',
-			'swf'			: {
-			'wmode'				: 'transparent',
-			'allowfullscreen'	: 'true'
-			}
-		});
-
-		return false;
-	});
-
-
-
-});
+var gallery = [];
+$(".video").each(function (i) {
+    gallery[i] = {
+        href: $(this).attr("xlink:href"),
+        title: $(this).attr("title")
+    };
+    $(this).on("click", function () {
+        $.fancybox(gallery, {
+            index: i,
+            helpers: {
+                title: {
+                    type: 'inside',
+                    position: 'top',
+                }
+            },
+            openSpeed: 'slow',
+            closeSpeed: 'slow',
+            autoDimensions: true
+        });
+        return false;
+    }); // on
+}); // each
