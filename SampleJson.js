ad7fdf691a0249cf26c04c806aea4711d6c25e28
ieucourses <ieucourses@gmail.com>
2014-11-20 11:36:07 +0200

    st
    st

--- SampleJson.js
+++ SampleJson.js
@@ -1,7 +1,7 @@
 var sampleJson1 = {
     ToolTipPosition: "bottom",
-    data: [{ order: 1, Text: "Lecture 1", ToolTipText: "Step1-Foo", highlighted: true },
-        { order: 2, Text: "Lecture 2", ToolTipText: "Step2-Bar", highlighted: true },
-        { order: 3, Text: "Lecture 3", ToolTipText: "Step3-Baz", highlighted: false },
-        { order: 4, Text: "Lecture 4", ToolTipText: "Step4-Quux", highlighted: false }]
+    data: [{ order: 1, Text: "Lecture 1", ToolTipText: "Lecture 1: Introduction to Eclipse ", highlighted: true },
+        { order: 2, Text: "Lecture 2", ToolTipText: "Lecture 2: Hello World Example", highlighted: true },
+        { order: 3, Text: "Lecture 3", ToolTipText: "Lecture 3: JUnit and Unit Testing", highlighted: false },
+        { order: 4, Text: "Lecture 4", ToolTipText: "Lecture 4: Maven build Lifecycle", highlighted: false }]
 };
